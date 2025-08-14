195ffe91ee3dce8957f7a8c792270b10bfcae517
Wraith1357 <bradley.cleveland1357@gmail.com>
2025-08-14 13:21:34 -0500

    .03: successfully counting number of words in file, also test.py added for testing

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ def get_book_text(file_path):
 
 
 
-
-
+def find_num_words(text_contents):
+    words_list = []
+    words_list.extend(text_contents.split())
+    #print(words_list) test stuff
+    num_words = len(words_list)
+    #print(num_words)
+    return num_words
+"""
+took a little while for me to figure out why this was only giving me a 1 as the number of words.
+turns out, append only adds one item at a time, so i was adding the entire list made by split as 
+a single item. Make sure I know how these functions work before using them lol
+"""
 
 
 
 def main():
     text_contents = get_book_text("books/frankenstein.txt")
 
-    print(text_contents)
+    print(f"{find_num_words(text_contents)} words found in the document")
     
 
 
