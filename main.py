346e5f163376fe13116c3d028498d8c62c901c63
Wraith1357 <bradley.cleveland1357@gmail.com>
2025-08-21 23:01:45 -0500

    .05: Report finished in proper formatting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import find_num_words
 from stats import character_count
-
+from stats import sort_list_dict
 
 def get_book_text(file_path):
 
@@ -14,10 +14,22 @@ def get_book_text(file_path):
 def main():
     text_contents = get_book_text("books/frankenstein.txt")
 
-    print(f"{find_num_words(text_contents)} words found in the document")
+    #print(f"{find_num_words(text_contents)} words found in the document")
     
     char_count = character_count(text_contents)
-    print(char_count)
+    #print(char_count)
+
+    sorted_list = sort_list_dict(char_count)
+    #print(sorted_list)  #because I now have the full sorted list, I can stop using the previous test
+                        #   and start just making the format print
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {find_num_words(text_contents)} total words")
+    print("--------- Character Count -------")
+    for element in sorted_list:
+        print(f"{element["char"]}: {element["num"]}")
+    print("============= END ===============")
 
 
 
--- stats.py
+++ stats.py
@@ -32,3 +32,71 @@ def character_count(text_string):
     return char_dict
 
 
+
+def sort_list_dict(char_count):
+
+    """
+    char_keys = list(char_count.keys())
+    #print(char_keys) prints keys confirmed
+    alpha_char_keys = []
+
+    for char in char_keys:
+        if(char.isalpha()):
+            alpha_char_keys.append(char)
+
+    print(alpha_char_keys)
+    I did this first, but realized it weasn't really the right way to do what i wanted to do
+    """
+
+
+    alpha_dict = {}
+    for char in char_count:
+        if (char.isalpha()):
+            alpha_dict[char] = char_count[char]
+
+    #print(alpha_dict)
+
+
+    #now I need to make a sorted list of dictionaries, with each dictionary having 2 key value pairs
+    # the pairs should look something like this {"char": "b", "num": 2300}
+    list_dicts = []
+    for char in alpha_dict:
+        list_dicts.append({"char": char, "num": alpha_dict[char]})
+    #print("line breaker")
+    #print(list_dicts)
+
+    list_dicts.sort(reverse = True, key = sort_by_this)
+    #print("line breaker") 
+    #print("line breaker")
+    #print(list_dicts)
+    
+    #this is now sorting the list of dictionaries correctly, now I need to return the sorted list
+    #   instead of printing it directly from the function, and then format the print properly within
+    #   main
+    return list_dicts
+    """
+    for char in char_keys:
+        if(char.isalpha() != True):
+            char_keys.remove(char)      Not really sure what i did wrong here, might be retarded right now
+    print(char_keys)                    but oh well. the first for loop only got rid of a few chars that
+    for char in char_keys:              needed to be removed from the list, but not even most of them.
+        if(char.isalpha() != True):     so, I just redid the same for loop multiple mtimes and eventually 
+            char_keys.remove(char)      there were enough loops that it got rid of everything that should
+    print(char_keys)                    have been removed, but that looks dumb and I know it shouldn't be
+    for char in char_keys:              working like that, so I just decided making a new list would be
+        if(char.isalpha() != True):     easier than figuring out what was wrong, and it was.
+            char_keys.remove(char)
+    print(char_keys)
+    for char in char_keys:
+        if(char.isalpha() != True):
+            char_keys.remove(char)
+    print(char_keys)
+    for char in char_keys:
+        if(char.isalpha() != True):
+            char_keys.remove(char)
+    print(char_keys)
+    """
+
+def sort_by_this(items):
+    return items["num"]
+
